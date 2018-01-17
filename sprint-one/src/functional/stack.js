a918c5206937cb8274ac389df5719bbffbc07ab7
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-01-17 11:58:36 -0800

    finished stack class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var storageSize = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storageSize++;
+    storage[storageSize] = value;
   };
 
   someInstance.pop = function() {
+    returnValue = storage[storageSize]
+    delete storage[storageSize];
+    storageSize = Math.max(storageSize -= 1, 0);
+    return returnValue;
   };
 
   someInstance.size = function() {
+    return storageSize;
   };
 
   return someInstance;
