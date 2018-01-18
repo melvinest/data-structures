710d042856728e0c4d90196eb2c14b694459b05b
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-01-17 17:37:05 -0800

    finished prototypal class

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  var storageSize = 0;
+  
+  var someInstance = Object.create(queueMethods);
+
+  someInstance.storageSize = storageSize;
+  someInstance.storage = storage;
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
 
+var queueMethods = {
+  enqueue: function(value) {
+    
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  },
 
+  dequeue: function() {
+    var dequeuedValue = this.storage[0];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+
+    for(var i = 1; i <= this.storageSize && this.storageSize > 0; i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+
+    delete this.storage[this.storageSize + 1];
+    return dequeuedValue;
+  },
+
+  size: function() {
+    return this.storageSize;
+  }
+
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,40 @@
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //debugger
+  var storage = {};
+  var storageSize = 0;
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = storage;
+  someInstance.storageSize = storageSize;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+
+  // Implement the methods below
+  push: function(value) {
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  },
+
+  pop: function() {
+    var index = this.storageSize - 1;
+    returnValue = this.storage[index];
+    delete this.storage[index];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+    return returnValue;
+  },
+
+  size: function() {
+    return this.storageSize;
+  }
+};
+
+
 
 
