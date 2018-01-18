b1868f1d2db6de9db456ee0029a7dc5dd99d63c4
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-01-17 18:10:35 -0800

    finished es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storageSize = 0;
   }
 
+  enqueue(value) {
+
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  }
+
+  dequeue() {
+    var dequeuedValue = this.storage[0];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+
+    for(var i = 1; i <= this.storageSize && this.storageSize > 0; i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+
+    delete this.storage[this.storageSize + 1];
+    return dequeuedValue;
+  }
+
+  size() {
+    return this.storageSize;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storageSize = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  }
+
+  pop(){
+    var index = this.storageSize - 1;
+    var returnValue = this.storage[index];
+    delete this.storage[index];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+    return returnValue;
+  }
+
+  size() {
+    return this.storageSize;
+  }
 }
