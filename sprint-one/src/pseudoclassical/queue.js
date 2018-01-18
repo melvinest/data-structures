7b1518a82cf1e9b184f8cdbe40771f7b24bf024e
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-01-17 17:38:01 -0800

    finished pseudoclassical class

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storageSize = 0;
+  this.storage = {};
+
 };
 
 
+queueMethods = {
+  enqueue: function(value) {
+    
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  },
+
+  dequeue: function() {
+    var dequeuedValue = this.storage[0];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+
+    for(var i = 1; i <= this.storageSize && this.storageSize > 0; i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+
+    delete this.storage[this.storageSize + 1];
+    return dequeuedValue;
+  },
+
+  size: function() {
+    return this.storageSize;
+  }
+
+};
+
+for(var key in queueMethods) {
+	Queue.prototype[key] = queueMethods[key];
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,39 @@
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.storageSize = 0;
+
 };
+
+
+stackMethods = {
+
+  // Implement the methods below
+  push: function(value) {
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  },
+
+  pop: function() {
+    var index = this.storageSize - 1;
+    returnValue = this.storage[index];
+    delete this.storage[index];
+    this.storageSize = Math.max(this.storageSize -= 1, 0);
+    return returnValue;
+  },
+
+  size: function() {
+    return this.storageSize;
+  }
+};
+
+for(var key in stackMethods) {
+	Stack.prototype[key] = stackMethods[key];
+};
+
+
 
 
